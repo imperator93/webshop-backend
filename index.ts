2eb68dcd790d90a3b5b00970b807023a83ca4f19
stojanov bizon <leobinbauer@gmail.com>
2024-09-16 14:09:12 +0200

    1

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Errback } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
